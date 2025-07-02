e9e3ae36e59f77c7ea6bc208bfe935145b76c651
FrancescoSls97 <frankds1997@gmail.com>
2025-07-02 11:48:47 +0200

    add error message to show route

--- routes/postsRouters.js
+++ routes/postsRouters.js
@@ -11,6 +11,12 @@ router.get("/", function (req, res) {
 router.get("/:id", function (req, res) {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.json({
+      error: "Not Found",
+      message: "post non trovato",
+    });
+  }
   res.json(post);
 });
 module.exports = router;
