797fa5ac55c9abff6748283f2986b3fc82cba446
FrancescoSls97 <frankds1997@gmail.com>
2025-07-02 16:19:25 +0200

    add controller for routers

---
+++ controllers/postsController.js
@@ -0,0 +1,80 @@
+const posts = require("../data/posts");
+
+const index = function (req, res) {
+  console.log(req.query);
+  let filteredPost = posts;
+  if (req.query.title) {
+    filteredPost = posts.filter((post) => post.title.includes(req.query.title));
+  }
+  res.json(filteredPost);
+};
+
+const show = function (req, res) {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    res.status(404);
+    return res.json({
+      error: "Not Found",
+      message: "post non trovato",
+    });
+  }
+  res.json(post);
+};
+
+const destroy = function (req, res) {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    res.status(404);
+    return res.json({
+      error: "Not Found",
+      message: "post non trovato",
+    });
+  }
+  posts.splice(posts.indexOf(post), 1);
+  console.log(posts);
+
+  res.sendStatus(204);
+};
+
+const store = function (req, res) {
+  const newId = posts[posts.length - 1].id + 1;
+  const newPost = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    image: req.body.image,
+    tags: req.body.tags,
+  };
+  posts.push(newPost);
+  console.log(posts);
+  res.status(201);
+  res.json(newPost);
+};
+
+const update = function (req, res) {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    res.status(404);
+    return res.json({
+      error: "Not Found",
+      message: "post non trovato",
+    });
+  }
+  post.title = req.body.title;
+  (post.content = req.body.content), (post.image = req.body.image);
+  post.tags = req.body.tags;
+
+  console.log(posts);
+
+  res.json(post);
+};
+module.exports = {
+  index,
+  show,
+  destroy,
+  store,
+  update,
+};
--- routes/postsRouters.js
+++ routes/postsRouters.js
@@ -1,76 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const posts = require("../data/posts");
-
+const postsController = require("../controllers/postsController");
 //index
-router.get("/", function (req, res) {
-  res.json(posts);
-});
+router.get("/", postsController.index);
 
 //show
-router.get("/:id", function (req, res) {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    res.status(404);
-    return res.json({
-      error: "Not Found",
-      message: "post non trovato",
-    });
-  }
-  res.json(post);
-});
+router.get("/:id", postsController.show);
 
 //destroy
-router.delete("/:id", function (req, res) {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    res.status(404);
-    return res.json({
-      error: "Not Found",
-      message: "post non trovato",
-    });
-  }
-  posts.splice(posts.indexOf(post), 1);
-  console.log(posts);
-
-  res.sendStatus(204);
-});
+router.delete("/:id", postsController.destroy);
 
 //store
-router.post("/", function (req, res) {
-  const newId = posts[posts.length - 1].id + 1;
-  const newPost = {
-    id: newId,
-    title: req.body.title,
-    content: req.body.content,
-    image: req.body.image,
-    tags: req.body.tags,
-  };
-  posts.push(newPost);
-  console.log(posts);
-  res.status(201);
-  res.json(newPost);
-});
+router.post("/", postsController.store);
 
 //update
-router.put("/:id", function (req, res) {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    res.status(404);
-    return res.json({
-      error: "Not Found",
-      message: "post non trovato",
-    });
-  }
-  post.title = req.body.title;
-  (post.content = req.body.content), (post.image = req.body.image);
-  post.tags = req.body.tags;
-
-  console.log(posts);
-
-  res.json(post);
-});
+router.put("/:id", postsController.update);
 module.exports = router;
